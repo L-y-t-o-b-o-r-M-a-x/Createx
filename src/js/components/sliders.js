cb082a5c2431b4dd9ae56a41f42ea05acf070f86
Max Lagutin <L-u-t-o-b-o-r-M-a-x@yandex.ru>
2023-05-01 11:38:44 +0300

    Доработка swiper, начало работы над section partners, testimonials

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -7,36 +7,67 @@ const portSlider = document.querySelector(".portfolio-section__items");
 const bodyStyles = window.getComputedStyle(document.body); // Получаем доступ к переменной '--grid-gap'
 const gap = parseInt(bodyStyles.getPropertyValue("--grid-gap"));
 
-const portfolioSlider = new Swiper(portSlider, {
-  slidesPerView: "3",
-  spaceBetween: gap,
-  loop: true,
-  on: {
-    init: function () {
-      console.log("swiper initialized");
+if (portSlider) {
+  const portfolioSlider = new Swiper(portSlider, {
+    slidesPerView: 1,
+    spaceBetween: gap,
+    on: {
+      init: function () {
+        const activeSlide = portSlider.querySelector('.swiper-slide-active');
 
-      const activeSlide = portSlider.querySelectorAll(".swiper-slide-active"); // Ищем первый слайд с '.swiper-slide-active'
+        const nextActiveSlide = activeSlide.nextElementSibling;
 
-      const nextActiveSlide = activeSlide.nextElementSibling; // Ищем сдедующий(2) слайд с помощью nextElementSibling
+        const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
 
-      const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
-
-      activeSlide.classList.add("slider-visible");
-      nextActiveSlide.classList.add("slider-visible");
-      nextNextActiveSlide.classList.add("slider-visible");
-
-      console.log(activeSlide);
-      console.log(nextActiveSlide);
-      console.log(nextNextActiveSlide);
+        activeSlide.classList.add('slider-visible');
+        nextActiveSlide.classList.add('slider-visible');
+        nextNextActiveSlide.classList.add('slider-visible');
+      },
     },
-  },
+    navigation: {
+      nextEl: '.portfolio-section__next',
+      prevEl: '.portfolio-section__prev',
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2
+      },
+      768: {
+        slidesPerView: 3,
+      }
+    }
+  });
 
-  navigation: {
-    nextEl: ".portfolio-section__next",
-    prevEl: ".portfolio-section__prev",
-  },
-});
+  document.querySelector('.portfolio-section__prev').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-next');
 
-portfolioSlider.on("slideChange", function () {
-  console.log("slide changed");
-});
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    if (activeSlide.previousElementSibling) {
+      const nextActiveSlide = activeSlide.previousElementSibling;
+      activeSlide.classList.add('slider-visible');
+      nextActiveSlide.classList.add('slider-visible');
+      activeSlide.nextElementSibling.classList.add('slider-visible');
+    }
+  });
+
+  document.querySelector('.portfolio-section__next').addEventListener('click', () => {
+    const activeSlide = portSlider.querySelector('.swiper-slide-active');
+
+    const nextActiveSlide = activeSlide.nextElementSibling;
+
+    const nextNextActiveSlide = nextActiveSlide.nextElementSibling;
+
+    document.querySelectorAll('.portfolio-section__items .swiper-slide').forEach(el => {
+      el.classList.remove('slider-visible');
+    });
+
+    activeSlide.classList.add('slider-visible');
+    nextActiveSlide.classList.add('slider-visible');
+    nextNextActiveSlide.classList.add('slider-visible');
+  });
+}
+
+
